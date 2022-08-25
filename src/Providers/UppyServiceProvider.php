ce850d907b4c3306e0500d54bc93d777917a3d83
xeroctation <umidakosimova.uk@gmail.com>
2022-08-25 17:44:35 +0500

    UppyServiceProvider update

--- src/Providers/UppyServiceProvider.php
+++ src/Providers/UppyServiceProvider.php
@@ -14,7 +14,7 @@ class UppyServiceProvider extends ServiceProvider
     public function register()
     {
         $this->publishes([
-            __DIR__ . '/../resources/views' => base_path('/resources/views'),
+            __DIR__ . '/../resources/views/components' => base_path('resources/views/components'),
             __DIR__ . '/../View/Components/' => base_path('app/View/Components'),
         ]);
     }
